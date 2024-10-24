0c0c782350baf1f43565c99cc32f52dbbb3c13df
Diego Saavedra <dsaavedra88@gmail.com>
2024-10-23 22:03:09 -0500

    add solution

--- ejercicios/1_intro.py
+++ ejercicios/1_intro.py
@@ -3,3 +3,19 @@
 1. Crear un ejercicio que me permita saber si una persona es mayor
 de edad.
 """
+
+
+
+
+
+# Solicitar el año de nacimiento al usuario
+anio_nacimiento = int(input("¿Cuál es tu año de nacimiento? (AAAA): "))
+
+# Obtener el año actual
+anio_actual = 2024
+
+# Calcular la edad
+edad = anio_actual - anio_nacimiento
+
+# Imprimir resultado
+print("Eres mayor de edad." * (edad >= 18) + "No eres mayor de edad." * (edad < 18))
