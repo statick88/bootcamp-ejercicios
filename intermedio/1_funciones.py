8cdb74b5971111ac7110c8bd3f19fbff192e87da
Diego Saavedra <dsaavedra88@gmail.com>
2024-10-28 22:54:47 -0500

    add intro

---
+++ intermedio/1_funciones.py
@@ -0,0 +1,27 @@
+# Funciones
+
+# def saludar(nombre):
+#     return "Hola", nombre
+
+# llamada = saludar("Diego")
+# print(llamada)
+
+# def media(nota1, nota2, nota3):
+#     return (nota1 + nota2 + nota3)/3
+
+# notaFinal = media(10, 8, 8.5)
+# print(notaFinal)
+
+# def sumar(num1, num2):
+#     resultado = num1 + num2
+#     # return
+#     return resultado
+
+# print(sumar(5, 3))
+
+def sumar(num1, num2):
+    resultado = num1 + num2
+    # return
+    return resultado
+
+print(sumar(5, 3))
